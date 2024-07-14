4b4a9309033483f0d5da7ae68f0e2bc9670a7fc9
cbclemmer <coltonclemmerdev@gmail.com>
2024-07-14 13:54:06 -0500

    Remove useless control folder

--- Control/lib.py
+++
@@ -1,9 +0,0 @@
-def get_api_key():
-	if not os.path.exists('config.json'):
-		raise Exception("Could not find config.json")
-	with open('config.json', 'r', encoding='utf-8') as f:
-		config = json.load(f)
-
-	if not 'openai_api_key' in config:
-		raise Exception('Could not find key: openai_api_key in config')
-	return config['openai_api_key']
